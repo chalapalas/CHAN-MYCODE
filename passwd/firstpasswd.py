47464dc4ddc370a89ea94bb9a02216b0d1b05caa
chalapalas <chalapalas@gmail.com>
2019-01-25 10:33:08 -0500

    secure password updated

---
+++ passwd/firstpasswd.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3 
+
+import getpass  # A simple Python program to demonstrate getpass.getpass() to read password import getpass
+
+p = getpass.getpass() 
+print('Password entered:', p)
+
---
+++ passwd/secondpasswd.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+## Try a real world test with getpass
+
+## import paramiko so we can talk SSH
+import paramiko # allows python to ssh
+import os # low level operating system commands
+import getpass # we need this to accept passwords
+
+## where to connect to
+t = paramiko.Transport("10.10.2.3", 22) ## IP and port of bender
+
+## how to connect (see other labs on using id_rsa private / public keypairs)
+t.connect(username="bender", password=getpass.getpass()) # notice the password references getpass
+
+## Make an sftp connection object
+sftp = paramiko.SFTPClient.from_transport(t)
+
+## copy our firstpasswd.py script to bender
+sftp.put("firstpasswd.py", "firstpasswd.py") # move file to target location home directory
+
+## close the connection
+sftp.close() # close the connection
+
